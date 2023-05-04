06831ee81d6cf88e286734050ea6ebb59a5c54b5
Oussama_LD <oussama.labiod@inttic.dz>
2023-05-04 11:57:47 +0100

    finish landing page and register and login and init dashboard

---
+++ clients/starup-app/src/Component/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@mui/material'
+function Dashboard() {
+    return (
+        <Box>
+            Dashboard page
+        </Box>
+    )
+}
+
+export default Dashboard
